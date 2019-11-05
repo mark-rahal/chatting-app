74be58ce441adc11b0e16a256be2e9c628781a27
Mark Rahal <mgr0904@gmail.com>
2019-11-04 16:26:03 -0800

    wip

--- TCPClient.py
+++ TCPClient.py
@@ -1,49 +1,96 @@
 import json
 from socket import *
+from threading import Thread
+
+userId = None
+users = None
 
 
-def getUserList(socket):
-    socket.send(json.dumps({'msgType': 1}).encode())
-    return json.loads(socket.recv(1024).decode())['users']
+def receiveMessageLoop(socket):
+    global users
 
+    while 1:
+        msg = socket.recv(1024)
+        msg = json.loads(msg.decode())
+        msgType = msg['msgType']
 
-def chatRequest(socket, chatWith):
-    socket.send(json.dumps({'msgType': 2, 'chatWith': chatWith}).encode())
-    return json.loads(socket.recv(1024).decode())
+        if (msgType == 1):
+            # updated users event
+            users = msg['users']
+            print("users updated")
+
+        elif (msgType == 2):
+            otherUserId = msg['id']
+            print(users[otherUserId]['username'],
+                  'wants to chat with you, enter 2')
+
+        elif (msgType == 4):
+            # direct message received
+
+            print(msg['username'] + ': ' + msg['msg'])
+            if (msg['msg'] == 'exit'):
+                print(msg['username'] + ' left the chat.')
+                # break
 
 
 def run():
-    userId = None
-    serverName = 'localhost'
+    global userId
+    global users
+
+    serverName = '127.0.0.1'
     serverPort = 12000
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
     username = input('Enter your username: ')
     clientSocket.send(json.dumps(
         {'msgType': 0, 'username': username}).encode())
-    welcomeObj = json.load(clientSocket.recv(1024).decode())
+    welcomeRes = clientSocket.recv(1024).decode()
+    welcomeObj = json.loads(welcomeRes)
     print(welcomeObj['msg'])
     userId = welcomeObj['id']
+
+    Thread(target=receiveMessageLoop, args=[clientSocket]).start()
+
     while 1:
         selectedAction = int(promptAction())
         if (selectedAction == 1):
-            users = getUserList(clientSocket)
-            for user in users:
-                print(user, end=': ')
-                if (users[user].get('chattingWith') == None):
-                    print('Available')
-                else:
-                    print('Chatting with ' + user)
+            if (len(users) == 1):
+                print('No other users online.')
+            else:
+                for user in users:
+                    if (user != userId):
+                        print(users[user]['username'], end=': ')
+                        if (users[user]['chatWith'] == None):
+                            print('Available')
+                        else:
+                            print('Chatting with ' + user)
         elif (selectedAction == 2):
-            users = getUserList(clientSocket)
-            for user in users:
-                if (users[user].get('chattingWith') == None):
-                    print(user, end=': ')
-                    print('Available')
-            chatWith = input(
-                'Enter the username of who you want to chat with: ')
+            if (len(users) == 1):
+                print('No other users online.')
+            else:
+                for user in users:
+                    if (user != userId):
+                        if (users[user]['chatWith'] == None):
+                            print(users[user]['username'], end=': ')
+                            print('Available')
+                chatWith = input(
+                    'Enter the username of who you want to chat with: ')
+                for tempId, user in users.items():
+                    if (user['username'] == chatWith):
+                        clientSocket.send(json.dumps(
+                            {'msgType': 2, 'chatWith': tempId}).encode())
+                        break
+
+                while 1:
+                    # asdf = True
+                    chatMsg = input('chat (type exit to exit): ')
+                    clientSocket.send(json.dumps(
+                        {'msgType': 4, 'msg': chatMsg}).encode())
+
         elif (selectedAction == 3):
-            print('bleh')
+            print('Goodbye.')
+            clientSocket.send(json.dumps({'msgType': 3}).encode())
+            break
         else:
             print('Please enter a valid option.')
     clientSocket.close()
--- TCPServer.py
+++ TCPServer.py
@@ -3,25 +3,64 @@ from socket import *
 from threading import Thread
 
 users = {}
+userToSocket = {}
 numUsers = 0
+
+
+def syncUsers():
+    global userToSocket
+
+    for sock in userToSocket.values():
+        sock.send(json.dumps({'msgType': 1, 'users': users}).encode())
 
 
 def handleConnection(socket):
     global numUsers
+    global users
+    global userToSocket
+
+    userId = str(numUsers)
+    userToSocket[userId] = socket
     while 1:
-        msg = json.loads(socket.recv(1024).decode())
-        if (msg['msgType'] == 0):
+        msg = socket.recv(1024)
+        msg = json.loads(msg.decode())
+        print(msg)
+
+        msgType = msg['msgType']
+        if (msgType == 0):
+            # msg type 0 = register user
             welcomeMsg = json.dumps(
-                {'id': numUsers, 'msg': 'Welcome, ' + msg['username']})
-            users[msg['username']] = {"id": numUsers, "chatWith": None}
+                {'id': userId, 'msg': 'Welcome, ' + msg['username']})
+            users[userId] = {"username": msg['username'],  "chatWith": None}
             numUsers += 1
             print(users)
             socket.send(welcomeMsg.encode())
-        elif (msg['msgType'] == 1):
-            socket.send(json.dumps({'users': users}).encode())
-        elif (msg['msgType'] == 2):
-            print(msg['chatWith'])
-            socket.send(json.dumps({'status': 'connected'}).encode())
+            syncUsers()
+        elif (msgType == 2):
+            # msg type 2 = chat request
+            print(msg)
+            otherUserId = msg['chatWith']
+            users[userId]['chatWith'] = otherUserId
+            syncUsers()
+            userToSocket[otherUserId].send(json.dumps(
+                {'msgType': 2, 'id': userId}).encode())
+        elif(msgType == 3):
+            # msg type 3 = close connection
+            break
+        elif(msgType == 4):
+            # msg type 4 = receive chat msg
+            print(userToSocket)
+            text = msg['msg']
+            otherUserId = users[userId]['chatWith']
+            socket.send(
+                json.dumps({'msgType': 4, 'username': users[userId]['username'], 'msg': text}).encode())
+            userToSocket[otherUserId].send(
+                json.dumps({'msgType': 4, 'username': users[userId]['username'], 'msg': text}).encode())
+            if (text == 'exit'):
+                # set chatwith to None for both users
+                print("blah")
+    users.pop(userId)
+    syncUsers()
     socket.close()
 
 
